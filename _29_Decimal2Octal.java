fd0f0e3d3ff437624163d66480558f6a86263f4a
Shekhar <rkshekhar015@gmail.com>
2021-09-19 21:13:44 +0530

    Decimal to Octal

--- _29_Decimal2Octal.java
+++ _29_Decimal2Octal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Decimal2Octal {
+public class _29_Decimal2Octal {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
