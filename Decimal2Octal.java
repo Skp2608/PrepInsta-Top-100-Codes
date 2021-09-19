a66deb15ece1330357afe3f95869f8169097d22d
Shekhar <rkshekhar015@gmail.com>
2021-09-19 20:11:11 +0530

    Decimal to octal

---
+++ Decimal2Octal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Decimal2Octal {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        System.out.println("Octal Number is "+Solution(n));
+    }
+
+    static int Solution(int n) {
+        int ans = 0;
+        int i = 1;
+        while (n > 0) {
+            ans += i * (n % 8);
+            i *= 10;
+            n /= 8;
+        }
+        return ans;
+    }
+}
